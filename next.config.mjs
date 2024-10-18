1a3ab031d5a2774af59328b7513e8985ce9255bb
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-10-19 03:23:57 +0530

    Edited image: remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["firebasestorage.googleapis.com"] },
+  images: {
+    // domains: ["firebasestorage.googleapis.com"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/v0/b/chatapp-4ef20.appspot.com/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
