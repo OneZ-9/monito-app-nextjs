90fca0ca576cb6cad91c54574aa34d7602562621
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-10-22 04:31:30 +0530

    CSS Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+      center: true,
+    },
     extend: {
       colors: {
         primary: {
