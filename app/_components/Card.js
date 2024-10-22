c2e3e9bd74ccb9a0420b3cb7356b570fc46dff49
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-10-22 22:13:42 +0530

    Modified Card componnent to add styles externally

--- app/_components/Card.js
+++ app/_components/Card.js
@@ -1,27 +1,32 @@
 import Image from "next/image";
 
-function Card({ children = null }) {
-  return (
-    <article className="flex w-[280px] flex-col gap-[8px] overflow-hidden rounded-[12px] bg-neutral-0 p-[8px] shadow-[0px_4px_28px_-2px_rgba(0,0,0,0.08)] transition-transform duration-300 hover:scale-105 max-lg:w-[185px]">
-      {children}
-    </article>
-  );
+function Card({
+  children = null,
+  className = "flex md:w-[220px] lg:w-[280px] flex-col gap-[8px] overflow-hidden rounded-[12px] bg-neutral-0 p-[8px] shadow-[0px_4px_28px_-2px_rgba(0,0,0,0.08)] transition-transform duration-300 hover:scale-105 w-[185px] ",
+}) {
+  return <article className={`${className}`}>{children}</article>;
 }
 
-function CardImage({ src, alt }) {
+function CardImage({
+  src,
+  alt,
+  height = 264,
+  width = 264,
+  className = "aspect-square rounded-[10px] object-cover",
+}) {
   return (
     <Image
       src={src}
       alt={alt}
-      width={264}
-      height={264}
-      className="aspect-square rounded-[10px] object-cover"
+      width={width}
+      height={height}
+      className={`${className}`}
     />
   );
 }
 
-function CardContent({ children = null }) {
-  return <div className="p-[8px_8px_20px]">{children}</div>;
+function CardContent({ children = null, className = "p-[8px_8px_20px]" }) {
+  return <div className={`${className}`}>{children}</div>;
 }
 
 export { Card, CardImage, CardContent };
